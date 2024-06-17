2da57ae086bf6aeca8cfac3893d6b8c044eca716
Kostya <65129914+cklyne@users.noreply.github.com>
2024-06-17 09:23:25 +0200

    Loss implemintation
    Implemented log-likelihood loss - Partial
    Added file for future loss-tests implementation

---
+++ tests/test_ml/test_loss/test_log_likelihood_loss.py
@@ -0,0 +1,12 @@
+import pytest
+from abc import ABC, abstractmethod
+from typing import Any, Union, Optional
+from collections.abc import Iterable
+from jax import numpy as jnp
+from jax import Array
+from jax.typing import ArrayLike
+import pathlib
+
+
+if __name__ == '__main__':
+    pytest.main()
