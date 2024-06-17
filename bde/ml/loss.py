cda1a37e630701b832b0e1ca812f1eadfa7e6c24
Kostya <65129914+cklyne@users.noreply.github.com>
2024-06-17 20:05:02 +0200

    Log-likelihood loss
    Added reduction to log-likelihood loss
    Fixed loss-issue where it did not compile due to:
     - Bad padding argument (all had to be provided in the same format)
     - Calling self where class is not tree-type (changed to dataclass)
     - Bad sigma argument where it is provided as a float (converts to an array)
    implemented tests for class.

--- bde/ml/loss.py
+++ bde/ml/loss.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import Any, Union, Optional
 from collections.abc import Iterable, Generator, Callable
+from flax.struct import dataclass
 import jax
 from jax import numpy as jnp
 from jax import Array
@@ -10,15 +11,10 @@ import pytest
 
 # from bde.utils import configs as cnfg
 
-
+@dataclass
 class LogLikelihoodLoss:
-    def __init__(
-            self,
-            sigma: float = 1e-6,
-            mean_weight: float = 1.0,
-    ):
-        self.sigma = sigma
-        self.mean_weight = mean_weight
+    sigma: float = 1e-6,
+    mean_weight: float = 1.0,
 
     @jax.jit
     def __call__(self, y_true: ArrayLike, y_pred: ArrayLike) -> ArrayLike:
@@ -32,7 +28,7 @@ class LogLikelihoodLoss:
         res = jnp.log(std_pred)
         res += (self.mean_weight / 2) * (((y_true - mean_pred) / std_pred) ** 2)
         # ADD: reductions
-        return res
+        return res.mean(axis=tuple(range(1, res.ndim)))
 
     @jax.jit
     def _split_pred(self, y_true: ArrayLike, y_pred: ArrayLike) -> tuple[Array, Array]:
@@ -56,11 +52,13 @@ class LogLikelihoodLoss:
         n_mean = y_true.shape[-1]
         mean_pred = y_pred[..., :n_mean]
         std_pred = y_pred[..., n_mean:(2 * n_mean)]
-        std_pred = jnp.clip(std_pred, a_min=self.sigma)
+        std_pred = jnp.clip(std_pred, a_min=jnp.array(self.sigma), a_max=None)
         n_std = std_pred.shape[-1]
+
+        padding = [(0, 0) if ax != y_true.ndim - 1 else (0, n_mean - n_std) for ax in range(y_true.ndim)]
         std_pred = jnp.pad(
             std_pred,
-            pad_width=[0 if ax != y_true.ndim - 1 else (0, n_mean - n_std) for ax in range(y_true.ndim)],
+            pad_width=padding,
             mode="constant",
             constant_values=1,
         )
