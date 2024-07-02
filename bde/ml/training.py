d8728f7409a8e6452dde3b71afa9d1f6e790b420
Kostya <65129914+cklyne@users.noreply.github.com>
2024-07-02 14:49:20 +0200

    Training step
    Changed training step implementation:
     - Simplified implementation
     - Made competible with standard loss output.
     - Changes to docstring and typehints.

--- bde/ml/training.py
+++ bde/ml/training.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from typing import Any, Union, Optional, Callable
 from collections.abc import Iterable
 import flax
+from flax.training.train_state import TrainState
 from flax import linen as nn
 from flax.struct import dataclass, field
 import jax
@@ -13,30 +14,31 @@ import pathlib
 # import bde
 import pytest
 
+import bde.ml.loss
+
 
 @jax.jit
-def train_step(state, inp: tuple[ArrayLike, ArrayLike],
-               f_loss: Callable[[Any, dict, Array], float]) -> tuple[Any, float]:
+def train_step(
+        state: TrainState,
+        batch: tuple[ArrayLike, ArrayLike],
+        f_loss: Callable[[ArrayLike, ArrayLike], float],
+) -> tuple[TrainState, float]:
     """
     Perform an optimization step for the network
-    :param state: The state of the network.
-    :param inp: Input points for the training set, containing 2 items:
-        - A set of training points.
-        - Points that should be near-0 valued for normalization.
+    :param state: The training-state of the network.
+    :param batch: Input data-points for the training set, containing 2 items:
+        - A set of training data-points.
+        - The corresponding labels.
     :param f_loss: The loss function used while training. Should have the following signature:
-        (state, params, batch: Array, boundary: Array, ...)
-    :return: Updated state of the network and the loss of the applied batch/ boundary combination
+        (y_true, y_pred)
+    :return: Updated state of the network and the loss.
     """
-    # Gradient function
-    grad_fn = jax.value_and_grad(f_loss,  # Function to calculate the loss
+    grad_fn = jax.value_and_grad(bde.ml.loss.flax_training_loss_wrapper_regression(f_loss=f_loss),
                                  argnums=1,  # Parameters are second argument of the function
-                                 has_aux=False  # used for functions with multiple outputs, like an extra metric
+                                 has_aux=False  # Function has additional outputs, here accuracy
                                  )
-    # Determine gradients for current model, parameters and batch
-    loss, grads = grad_fn(state, state.params, inp)
-    # Perform parameter update with gradients and optimizer
+    loss, grads = grad_fn(state, state.params, batch)
     state = state.apply_gradients(grads=grads)
-    # Return state and any other value we might want
     return state, loss
 
 
