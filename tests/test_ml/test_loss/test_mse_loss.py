45c72042d28a6f8e844460df98604f1dde5a69d8
Kostya <65129914+cklyne@users.noreply.github.com>
2024-08-28 11:52:48 +0200

    Improved readability
    Improved test structure for better readability.
    Changed global param name to all-caps.

--- tests/test_ml/test_loss/test_mse_loss.py
+++ tests/test_ml/test_loss/test_mse_loss.py
@@ -13,29 +13,25 @@ from bde.ml.loss import LossMSE
 from bde.utils import configs as cnfg
 
 SEED = cnfg.General.SEED
-possible_reductions = [True, False]
+POSSIBLE_REDUCTIONS = [True, False]
 
 
 class TestLossMSECall:
     @staticmethod
     @pytest.mark.parametrize("do_use_jit", [True, False])
-    @pytest.mark.parametrize("reduction", possible_reductions)
+    @pytest.mark.parametrize("reduction", POSSIBLE_REDUCTIONS)
     def test_no_std_like_mse(do_use_jit, reduction):
         n_batch, n_features = 10, 1
         key = jax.random.key(seed=SEED)
         key, subkey = jax.random.split(key)
         y_true = jax.random.normal(subkey, (n_batch, n_features))
+        expected_loss = (y_true ** 2).mean() if reduction else y_true.reshape((-1)) ** 2
         with jax.disable_jit(disable=not do_use_jit):
-            if reduction:
-                assert jnp.allclose(
-                    LossMSE(do_reduce=reduction)(y_true, jnp.zeros_like(y_true)),
-                    (y_true ** 2).mean(),
-                )
-            else:
-                assert jnp.allclose(
-                    LossMSE(do_reduce=reduction)(y_true, jnp.zeros_like(y_true)),
-                    y_true.reshape((-1)) ** 2,
-                )
+            f_loss = LossMSE(do_reduce=reduction)
+            assert jnp.allclose(
+                f_loss(y_true, jnp.zeros_like(y_true)),
+                expected_loss,
+            )
 
 
 if __name__ == '__main__':
