cc089160a7c2b4013d69c91d70e30dc7a9c15bda
SteffenJw <74009241+SteffenJw@users.noreply.github.com>
2024-10-08 21:23:35 +0200

    Update conf.py
    missing ' '

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ project = 'BDE'
 copyright = '2024, Konstantin and Steffen'
 author = 'Konstantin and Steffen'
 # release = get_version('bde')
-release = 0.1.0
+release = '0.1.0'
 version = ".".join(release.split(".")[:3])
 
 sys.path.insert(0, os.path.abspath('..'))
