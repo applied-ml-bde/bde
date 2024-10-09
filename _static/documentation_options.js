d9bcaef7282a5d56ca03f95e42e27deaf67ae708
cklyne <cklyne@users.noreply.github.com>
2024-10-09 22:17:47 +0000

    [ci skip] Merge pull request #42 from applied-ml-bde/examples
    Examples and minor fixes d7c490a8d643230c9f2b60227ef19e4665307335

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev302+gb938164.d20241009',
+    VERSION: '0.1.dev306+gd7c490a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
