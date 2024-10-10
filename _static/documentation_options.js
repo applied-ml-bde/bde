4cf8dd3a5f2f11548649942e1dbdc79f7e82a0f2
SteffenJw <SteffenJw@users.noreply.github.com>
2024-10-10 01:35:59 +0000

    [ci skip] Merge pull request #46 from applied-ml-bde/docs
    Update index.rst 33a4c50f32a4e752361256c18a6c86759c3424f9

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev325+g17f4fc3.d20241010',
+    VERSION: '0.1.dev327+g33a4c50.d20241010',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
