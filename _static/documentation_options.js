6e455a9863ed1c55759857c0c96a4d4c092dd0db
cklyne <cklyne@users.noreply.github.com>
2024-10-10 01:05:13 +0000

    [ci skip] Merge pull request #45 from applied-ml-bde/cklyne-patch-1
    Update api.rst 17f4fc3c7dc4c38a01e92bf556ac76e65624e92d

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev322+g33d7c0f.d20241010',
+    VERSION: '0.1.dev325+g17f4fc3.d20241010',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
